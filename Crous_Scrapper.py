c9d18b4af9516f0a42261c25611fcf5ed52bb739
Lyes Khoumeri <64961951+LyesYe@users.noreply.github.com>
2024-07-16 15:11:40 -0400

    update

--- Crous_Scrapper.py
+++ Crous_Scrapper.py
@@ -16,7 +16,7 @@ options.add_experimental_option('useAutomationExtension', False)
 
 browser = webdriver.Chrome(options=options)
 browser.implicitly_wait(5)  # seconds
-browser.get('https://trouverunlogement.lescrous.fr/tools/31/search?bounds=5.2286902_43.3910329_5.5324758_43.1696205')
+browser.get('https://trouverunlogement.lescrous.fr/tools/36/search?bounds=5.2286902_43.3910329_5.5324758_43.1696205')
 # browser.get('https://trouverunlogement.lescrous.fr/tools/31/search?bounds=6.7872143_47.6713057_6.8948707_47.6203259')
 
 
@@ -24,13 +24,11 @@ def test_chargement():
     page_source = browser.page_source  # Get the page source
 
     # Use a regular expression with optional 's' character
-    match = re.search(r'(\d+) logement(s?) trouvé(s?)', page_source)
+    match = re.search(r'CITE LUMINY', page_source)
 
     if match:
-        n = int(match.group(1))  # Extract the integer 'n' from the match
-        print(f"{n} logement{'s' if n > 1 else ''} trouvé{'s' if n > 1 else ''}")
         send_email_2_me(
-            f"CROUS AVAILIBLE ({n} logement{'s' if n > 1 else ''})", "GO NOW CROUS DISPO ! CORDIALMEENT")
+        f"CROUS AVAILIBLE CITE LUMINY", "GO NOW CROUS DISPO ! CORDIALMEENT")
     else:
         browser.refresh()
         time.sleep(20)
@@ -38,21 +36,22 @@ def test_chargement():
         test_chargement()
 
 
+
 def send_email_2_me(msg, sub):
     print(msg, sub)
-    mail = "UR EMAIL"
-    gmail_code = "UR GMAIL CODE"
+    mail = "croustest@outlook.com"
+    password = "Lyesipog@@13102001"
     msg = MIMEMultipart()
     msg['From'] = mail
     msg['To'] = mail
     msg['Subject'] = str(sub)
     message = str(msg)
     msg.attach(MIMEText(message))
-    mailserver = smtplib.SMTP('smtp.gmail.com', 587)
+    mailserver = smtplib.SMTP('smtp.office365.com', 587)
     mailserver.ehlo()
     mailserver.starttls()
     mailserver.ehlo()
-    mailserver.login(mail, gmail_code)
+    mailserver.login(mail, password)
     mailserver.sendmail(mail, mail, msg.as_string())
     mailserver.quit()
 
